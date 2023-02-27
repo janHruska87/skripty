ef11a70c93ac812017ef62b35adf93457f19905e
janhr <jan.hruska87@seznam.cz>
2023-02-27 13:45:11 +0100

    prvni komit

--- main.py
+++ main.py
@@ -3,14 +3,33 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+import os
+import img2pdf
+import pathlib
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def start(basePath):
+    if pathlib.Path(base_path).exists():
+        print("Soubor/Cesta existuje...")
+        files = os.listdir(basePath)
+        printAllFiles(files)
+    else:
+        print("Cesta, nebo soubor neexistuje...")
 
+
+def printAllFiles(files):
+    images = list()
+    for file in files:
+        if file.endswith("jpg"):
+            images.append(base_path + file)
+            print("Nacten soubor: " + base_path + file)
+
+    with open(base_path + "output.pdf",mode="w",encoding="utf-8") as f:
+        f.write(img2pdf.convert(images))
+        print("Zapisuji soubor:" + base_path + "output.pdf")
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    base_path = "C:\\Users\\janhr\\OneDrive\\Plocha\\PDF z obrazku\\"
+    start(base_path)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
